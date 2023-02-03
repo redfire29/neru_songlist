125459ab53987027bcf7f1e1d4344b2bda559413
redfire29 <redfire29@gmail.com.tw>
2023-02-03 11:54:04 +0800

    update

--- array.js
+++ array.js
@@ -7,43 +7,50 @@ const arrayData = [
     songList: [
       {
         showDate: true,
-        songName: "GHOST / 星街すいせい",
+        songName: "GHOST",
+        singer: "星街すいせい",
         songTime: "0:05:55",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=355s"
       },
       {
         showDate: true,
-        songName: "群青 / YOASOBI",
+        songName: "群青",
+        singer: "YOASOBI",
         songTime: "0:16:30",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=990s"
       },
       {
         showDate: true,
-        songName: "陪妳過假日 / Leo王 feat. 9m88",
+        songName: "陪妳過假日",
+        singer: "Leo王 feat. 9m88",
         songTime: "0:32:45",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=1965s"
       },
       {
         showDate: true,
-        songName: "Zombie / The Cranberries",
+        songName: "Zombie",
+        singer: "The Cranberries",
         songTime: "0:39:52",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2392s"
       },
       {
         showDate: true,
-        songName: "レディメイド / Ado",
+        songName: "レディメイド",
+        singer: "Ado",
         songTime: "0:48:46",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2926s"
       },
       {
         showDate: true,
-        songName: "月亮代表我的心 / 鄧麗君",
+        songName: "月亮代表我的心",
+        singer: "鄧麗君",
         songTime: "0:57:21",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3441s"
       },
       {
         showDate: true,
-        songName: "ダンシング・ヒーロー / 荻野目洋子",
+        songName: "ダンシング・ヒーロー",
+        singer: "荻野目洋子",
         songTime: "1:04:14",
         songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3854s"
       },
@@ -57,55 +64,64 @@ const arrayData = [
     songList: [
       {
         showDate: true,
-        songName: "GLAMOROUS SKY / 中島美嘉",
+        songName: "GLAMOROUS SKY",
+        singer: "中島美嘉",
         songTime: "0:08:08",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=488s"
       },
       {
         showDate: true,
-        songName: "Ref:rain / Aimer",
+        songName: "Ref:rain",
+        singer: "Aimer",
         songTime: "0:15:27",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=927s"
       },
       {
         showDate: true,
-        songName: "ドラえもんのうた / 大杉久美子",
+        songName: "ドラえもんのうた",
+        singer: "大杉久美子",
         songTime: "0:23:27",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1407s"
       },
       {
         showDate: true,
-        songName: "マリーゴールド / あいみょん",
+        songName: "マリーゴールド",
+        singer: "あいみょん",
         songTime: "0:31:00",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1860s"
       },
       {
         showDate: true,
-        songName: "Plastic Love / 竹内まりや",
+        songName: "Plastic Love",
+        singer: "竹内まりや",
         songTime: "0:37:59",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2279s"
       },
       {
         showDate: true,
-        songName: "桃色片想い / 松浦亜弥",
+        songName: "桃色片想い",
+        singer: "松浦亜弥",
         songTime: "0:45:25",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2725s"
       },
       {
         showDate: true,
-        songName: "ラブか / Ado",
+        songName: "ラブか",
+        singer: "Ado",
         songTime: "0:53:22",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3202s"
       },
       {
         showDate: true,
-        songName: "北国の春 / 千昌夫",
+        songName: "北国の春",
+        singer: "千昌夫",
         songTime: "0:59:41",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3581s"
       },
       {
         showDate: true,
-        songName: "God knows / 平野綾",
+        songName: "God knows",
+        singer: "平野綾",
         songTime: "1:09:55",
         songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=4195s"
       },
@@ -119,37 +135,43 @@ const arrayData = [
     songList: [
       {
         showDate: true,
-        songName: "超跑情人夢 / 卜學亮 (超喘情人夢)",
+        songName: "超跑情人夢",
+        singer: "卜學亮 (超喘情人夢)",
         songTime: "0:48:57",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=2937s"
       },
       {
         showDate: true,
-        songName: "怪物 / YOASOBI(加速板)",
+        songName: "怪物",
+        singer: "YOASOBI(加速板)",
         songTime: "0:57:14",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3434s"
       },
       {
         showDate: true,
-        songName: "你是我的花朵 / 伍佰&China Blue(加速板)",
+        songName: "你是我的花朵",
+        singer: "伍佰&China Blue(加速板)",
         songTime: "1:02:06",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3726s"
       },
       {
         showDate: true,
-        songName: "Faded / Alan Walker",
+        songName: "Faded",
+        singer: "Alan Walker",
         songTime: "1:11:58",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4318s"
       },
       {
         showDate: true,
-        songName: "fly me to the moon / Frank Sinatra",
+        songName: "fly me to the moon",
+        singer: "Frank Sinatra",
         songTime: "1:15:21",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4521s"
       },
       {
         showDate: true,
-        songName: "It's My Life / Bon Jovi",
+        songName: "It's My Life",
+        singer: "Bon Jovi",
         songTime: "1:19:22",
         songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4762s"
       },
@@ -163,37 +185,43 @@ const arrayData = [
     songList: [
       {
         showDate: true,
-        songName: "ギラギラ / Ado",
+        songName: "ギラギラ",
+        singer: "Ado",
         songTime: "0:07:35",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=455s"
       },
       {
         showDate: true,
-        songName: "KING / Kanaria",
+        songName: "KING",
+        singer: "Kanaria",
         songTime: "0:15:32",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=932s"
       },
       {
         showDate: true,
-        songName: "我還年輕 我還年輕 /老王樂隊",
+        songName: "我還年輕 我還年輕",
+        singer: "老王樂隊",
         songTime: "0:23:25",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1405s"
       },
       {
         showDate: true,
-        songName: "Rolling in the deep / Adele (內有好康)",
+        songName: "Rolling in the deep",
+        singer: "Adele (內有好康)",
         songTime: "0:32:45",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1965s"
       },
       {
         showDate: true,
-        songName: "無人熟識 / 張清芳(歸剛ㄟ)",
+        songName: "無人熟識",
+        singer: "張清芳(歸剛ㄟ)",
         songTime: "0:43:38",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=2618s"
       },
       {
         showDate: true,
-        songName: "うっせぇわ / ado",
+        songName: "うっせぇわ",
+        singer: "ado",
         songTime: "0:57:03",
         songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=3423s"
       },
@@ -207,25 +235,29 @@ const arrayData = [
     songList: [
       {
         showDate: true,
-        songName: "シル・ヴ・プレジデント / P丸様",
+        songName: "シル・ヴ・プレジデント",
+        singer: "P丸様",
         songTime: "0:50:13",
         songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3013s"
       },
       {
         showDate: true,
-        songName: "Make me wanna die / The Pretty Reckless",
+        songName: "Make me wanna die",
+        singer: "The Pretty Reckless",
         songTime: "0:56:31",
         songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3391s"
       },
       {
         showDate: true,
-        songName: "Last stardust / Aimer",
+        songName: "Last stardust",
+        singer: "Aimer",
         songTime: "1:00:43",
         songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3643s"
       },
       {
         showDate: true,
-        songName: "残酷な天使のテーゼ / 高橋洋子",
+        songName: "残酷な天使のテーゼ",
+        singer: "高橋洋子",
         songTime: "1:12:11",
         songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=4331s"
       },
