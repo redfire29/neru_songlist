868a548c5de82d873a92a365835bf91663d5e40e
redfire29 <redfire29@gmail.com.tw>
2023-02-03 11:44:06 +0800

    update array

--- array.js
+++ array.js
@@ -1,236 +1,234 @@
 const arrayData = [
-  [
-    {
-      date: "2021年12月11日",
-      streamName: "【歌枠】 是辣個兩千訂閱慶祝歌回！！！今天可以不要嘴ㄆㄧㄚˊ嗎",
-      showList: true,
-      showDate: true,
-      songList: [
-        {
-          showDate: true,
-          songName: "GHOST / 星街すいせい",
-          songTime: "0:05:55",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=355s"
-        },
-        {
-          showDate: true,
-          songName: "群青 / YOASOBI",
-          songTime: "0:16:30",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=990s"
-        },
-        {
-          showDate: true,
-          songName: "陪妳過假日 / Leo王 feat. 9m88",
-          songTime: "0:32:45",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=1965s"
-        },
-        {
-          showDate: true,
-          songName: "Zombie / The Cranberries",
-          songTime: "0:39:52",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2392s"
-        },
-        {
-          showDate: true,
-          songName: "レディメイド / Ado",
-          songTime: "0:48:46",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2926s"
-        },
-        {
-          showDate: true,
-          songName: "月亮代表我的心 / 鄧麗君",
-          songTime: "0:57:21",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3441s"
-        },
-        {
-          showDate: true,
-          songName: "ダンシング・ヒーロー / 荻野目洋子",
-          songTime: "1:04:14",
-          songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3854s"
-        },
-      ],
-    },
-    {
-      date: "2021年12月4日",
-      streamName: "【歌枠】 明天要考日檢只好靠歌唱學習法了！！！只有日文歌的日文歌回",
-      showList: true,
-      showDate: true,
-      songList: [
-        {
-          showDate: true,
-          songName: "GLAMOROUS SKY / 中島美嘉",
-          songTime: "0:08:08",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=488s"
-        },
-        {
-          showDate: true,
-          songName: "Ref:rain / Aimer",
-          songTime: "0:15:27",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=927s"
-        },
-        {
-          showDate: true,
-          songName: "ドラえもんのうた / 大杉久美子",
-          songTime: "0:23:27",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1407s"
-        },
-        {
-          showDate: true,
-          songName: "マリーゴールド / あいみょん",
-          songTime: "0:31:00",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1860s"
-        },
-        {
-          showDate: true,
-          songName: "Plastic Love / 竹内まりや",
-          songTime: "0:37:59",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2279s"
-        },
-        {
-          showDate: true,
-          songName: "桃色片想い / 松浦亜弥",
-          songTime: "0:45:25",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2725s"
-        },
-        {
-          showDate: true,
-          songName: "ラブか / Ado",
-          songTime: "0:53:22",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3202s"
-        },
-        {
-          showDate: true,
-          songName: "北国の春 / 千昌夫",
-          songTime: "0:59:41",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3581s"
-        },
-        {
-          showDate: true,
-          songName: "God knows / 平野綾",
-          songTime: "1:09:55",
-          songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=4195s"
-        },
-      ]
-    },
-    {
-      date: "2021年11月28日",
-      streamName: "【突發雜談】 ✞Trying NOT to speak Chinese in a half hour/30分中国語禁止",
-      showList: true,
-      showDate: true,
-      songList: [
-        {
-          showDate: true,
-          songName: "超跑情人夢 / 卜學亮 (超喘情人夢)",
-          songTime: "0:48:57",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=2937s"
-        },
-        {
-          showDate: true,
-          songName: "怪物 / YOASOBI(加速板)",
-          songTime: "0:57:14",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3434s"
-        },
-        {
-          showDate: true,
-          songName: "你是我的花朵 / 伍佰&China Blue(加速板)",
-          songTime: "1:02:06",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3726s"
-        },
-        {
-          showDate: true,
-          songName: "Faded / Alan Walker",
-          songTime: "1:11:58",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4318s"
-        },
-        {
-          showDate: true,
-          songName: "fly me to the moon / Frank Sinatra",
-          songTime: "1:15:21",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4521s"
-        },
-        {
-          showDate: true,
-          songName: "It's My Life / Bon Jovi",
-          songTime: "1:19:22",
-          songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4762s"
-        },
-      ],
-    },
-    {
-      date: "2021年11月27日",
-      streamName: "【歌枠】 闇音ネル✞歌回大雜燴✞",
-      showList: true,
-      showDate: true,
-      songList: [
-        {
-          showDate: true,
-          songName: "ギラギラ / Ado",
-          songTime: "0:07:35",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=455s"
-        },
-        {
-          showDate: true,
-          songName: "KING / Kanaria",
-          songTime: "0:15:32",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=932s"
-        },
-        {
-          showDate: true,
-          songName: "我還年輕 我還年輕 /老王樂隊",
-          songTime: "0:23:25",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1405s"
-        },
-        {
-          showDate: true,
-          songName: "Rolling in the deep / Adele (內有好康)",
-          songTime: "0:32:45",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1965s"
-        },
-        {
-          showDate: true,
-          songName: "無人熟識 / 張清芳(歸剛ㄟ)",
-          songTime: "0:43:38",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=2618s"
-        },
-        {
-          showDate: true,
-          songName: "うっせぇわ / ado",
-          songTime: "0:57:03",
-          songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=3423s"
-        },
-      ],
-    },
-    {
-      date: "2021年11月26日",
-      streamName: "【初配信】夜幕降臨! 歡迎來到ネル的晚宴✝ | 闇音ネル",
-      showList: true,
-      showDate: true,
-      songList: [
-        {
-          showDate: true,
-          songName: "シル・ヴ・プレジデント / P丸様",
-          songTime: "0:50:13",
-          songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3013s"
-        },
-        {
-          showDate: true,
-          songName: "Make me wanna die / The Pretty Reckless",
-          songTime: "0:56:31",
-          songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3391s"
-        },
-        {
-          showDate: true,
-          songName: "Last stardust / Aimer",
-          songTime: "1:00:43",
-          songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3643s"
-        },
-        {
-          showDate: true,
-          songName: "残酷な天使のテーゼ / 高橋洋子",
-          songTime: "1:12:11",
-          songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=4331s"
-        },
-      ],
-    }
-  ]
+  {
+    date: "2021年12月11日",
+    streamName: "【歌枠】 是辣個兩千訂閱慶祝歌回！！！今天可以不要嘴ㄆㄧㄚˊ嗎",
+    showList: true,
+    showDate: true,
+    songList: [
+      {
+        showDate: true,
+        songName: "GHOST / 星街すいせい",
+        songTime: "0:05:55",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=355s"
+      },
+      {
+        showDate: true,
+        songName: "群青 / YOASOBI",
+        songTime: "0:16:30",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=990s"
+      },
+      {
+        showDate: true,
+        songName: "陪妳過假日 / Leo王 feat. 9m88",
+        songTime: "0:32:45",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=1965s"
+      },
+      {
+        showDate: true,
+        songName: "Zombie / The Cranberries",
+        songTime: "0:39:52",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2392s"
+      },
+      {
+        showDate: true,
+        songName: "レディメイド / Ado",
+        songTime: "0:48:46",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=2926s"
+      },
+      {
+        showDate: true,
+        songName: "月亮代表我的心 / 鄧麗君",
+        songTime: "0:57:21",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3441s"
+      },
+      {
+        showDate: true,
+        songName: "ダンシング・ヒーロー / 荻野目洋子",
+        songTime: "1:04:14",
+        songLink: "https://www.youtube.com/watch?v=aV6aDJJBwow&t=3854s"
+      },
+    ],
+  },
+  {
+    date: "2021年12月4日",
+    streamName: "【歌枠】 明天要考日檢只好靠歌唱學習法了！！！只有日文歌的日文歌回",
+    showList: true,
+    showDate: true,
+    songList: [
+      {
+        showDate: true,
+        songName: "GLAMOROUS SKY / 中島美嘉",
+        songTime: "0:08:08",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=488s"
+      },
+      {
+        showDate: true,
+        songName: "Ref:rain / Aimer",
+        songTime: "0:15:27",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=927s"
+      },
+      {
+        showDate: true,
+        songName: "ドラえもんのうた / 大杉久美子",
+        songTime: "0:23:27",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1407s"
+      },
+      {
+        showDate: true,
+        songName: "マリーゴールド / あいみょん",
+        songTime: "0:31:00",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=1860s"
+      },
+      {
+        showDate: true,
+        songName: "Plastic Love / 竹内まりや",
+        songTime: "0:37:59",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2279s"
+      },
+      {
+        showDate: true,
+        songName: "桃色片想い / 松浦亜弥",
+        songTime: "0:45:25",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=2725s"
+      },
+      {
+        showDate: true,
+        songName: "ラブか / Ado",
+        songTime: "0:53:22",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3202s"
+      },
+      {
+        showDate: true,
+        songName: "北国の春 / 千昌夫",
+        songTime: "0:59:41",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=3581s"
+      },
+      {
+        showDate: true,
+        songName: "God knows / 平野綾",
+        songTime: "1:09:55",
+        songLink: "https://www.youtube.com/watch?v=lb7FiQEIUlE&t=4195s"
+      },
+    ]
+  },
+  {
+    date: "2021年11月28日",
+    streamName: "【突發雜談】 ✞Trying NOT to speak Chinese in a half hour/30分中国語禁止",
+    showList: true,
+    showDate: true,
+    songList: [
+      {
+        showDate: true,
+        songName: "超跑情人夢 / 卜學亮 (超喘情人夢)",
+        songTime: "0:48:57",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=2937s"
+      },
+      {
+        showDate: true,
+        songName: "怪物 / YOASOBI(加速板)",
+        songTime: "0:57:14",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3434s"
+      },
+      {
+        showDate: true,
+        songName: "你是我的花朵 / 伍佰&China Blue(加速板)",
+        songTime: "1:02:06",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=3726s"
+      },
+      {
+        showDate: true,
+        songName: "Faded / Alan Walker",
+        songTime: "1:11:58",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4318s"
+      },
+      {
+        showDate: true,
+        songName: "fly me to the moon / Frank Sinatra",
+        songTime: "1:15:21",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4521s"
+      },
+      {
+        showDate: true,
+        songName: "It's My Life / Bon Jovi",
+        songTime: "1:19:22",
+        songLink: "https://www.youtube.com/watch?v=Rk1upc7Mk1E&t=4762s"
+      },
+    ],
+  },
+  {
+    date: "2021年11月27日",
+    streamName: "【歌枠】 闇音ネル✞歌回大雜燴✞",
+    showList: true,
+    showDate: true,
+    songList: [
+      {
+        showDate: true,
+        songName: "ギラギラ / Ado",
+        songTime: "0:07:35",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=455s"
+      },
+      {
+        showDate: true,
+        songName: "KING / Kanaria",
+        songTime: "0:15:32",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=932s"
+      },
+      {
+        showDate: true,
+        songName: "我還年輕 我還年輕 /老王樂隊",
+        songTime: "0:23:25",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1405s"
+      },
+      {
+        showDate: true,
+        songName: "Rolling in the deep / Adele (內有好康)",
+        songTime: "0:32:45",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=1965s"
+      },
+      {
+        showDate: true,
+        songName: "無人熟識 / 張清芳(歸剛ㄟ)",
+        songTime: "0:43:38",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=2618s"
+      },
+      {
+        showDate: true,
+        songName: "うっせぇわ / ado",
+        songTime: "0:57:03",
+        songLink: "https://www.youtube.com/watch?v=bEiGy1POyYY&t=3423s"
+      },
+    ],
+  },
+  {
+    date: "2021年11月26日",
+    streamName: "【初配信】夜幕降臨! 歡迎來到ネル的晚宴✝ | 闇音ネル",
+    showList: true,
+    showDate: true,
+    songList: [
+      {
+        showDate: true,
+        songName: "シル・ヴ・プレジデント / P丸様",
+        songTime: "0:50:13",
+        songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3013s"
+      },
+      {
+        showDate: true,
+        songName: "Make me wanna die / The Pretty Reckless",
+        songTime: "0:56:31",
+        songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3391s"
+      },
+      {
+        showDate: true,
+        songName: "Last stardust / Aimer",
+        songTime: "1:00:43",
+        songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3643s"
+      },
+      {
+        showDate: true,
+        songName: "残酷な天使のテーゼ / 高橋洋子",
+        songTime: "1:12:11",
+        songLink: "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=4331s"
+      },
+    ],
+  }
 ];
