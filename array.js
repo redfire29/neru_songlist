832f48f39fc71e4c0f19840cc2438d4ef6b0559b
redfire29 <redfire29@gmail.com.tw>
2023-02-03 09:55:06 +0800

    update

--- array.js
+++ array.js
@@ -1,28 +1,30 @@
 const arrayData = [
   [
     {
-      "date": "2021年11月26日",
-      "songName": "シル・ヴ・プレジデント / P丸様",
-      "songTime": "0:50:13",
-      "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3013s"
-    },
-    {
-      "date": "2021年11月26日",
-      "songName": "Make me wanna die / The Pretty Reckless",
-      "songTime": "0:56:31",
-      "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3391s"
-    },
-    {
-      "date": "2021年11月26日",
-      "songName": "Last stardust / Aimer",
-      "songTime": "1:00:43",
-      "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3643s"
-    },
-    {
-      "date": "2021年11月26日",
-      "songName": "残酷な天使のテーゼ / 高橋洋子",
-      "songTime": "1:12:11",
-      "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=4331s"
-    },
+      date: "2021年11月26日",
+      streamName: "【初配信】夜幕降臨! 歡迎來到ネル的晚宴✝ | 闇音ネル",
+      songList: [
+        {
+          "songName": "シル・ヴ・プレジデント / P丸様",
+          "songTime": "0:50:13",
+          "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3013s"
+        },
+        {
+          "songName": "Make me wanna die / The Pretty Reckless",
+          "songTime": "0:56:31",
+          "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3391s"
+        },
+        {
+          "songName": "Last stardust / Aimer",
+          "songTime": "1:00:43",
+          "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=3643s"
+        },
+        {
+          "songName": "残酷な天使のテーゼ / 高橋洋子",
+          "songTime": "1:12:11",
+          "songLink": "https://www.youtube.com/watch?v=QKsYKCjRyMM&t=4331s"
+        },
+      ],
+    }
   ]
 ];
